5d06a1eb8abd8edb14ea11b1b0485f6e865af66f
Daniel Duran Gil <ddurangi@everis.com>
2021-08-30 12:40:30 +0200

    exported collection postman correct, removed init table real data

--- src/functional-test/java/com/andorrero/amaristestjava/EndPointTest.java
+++ src/functional-test/java/com/andorrero/amaristestjava/EndPointTest.java
@@ -5,7 +5,11 @@ import java.time.Month;
 import java.time.ZoneId;
 import java.util.Map;
 
-import org.junit.jupiter.api.Test;
+import com.andorrero.amaristestjava.builder.PriceEntityBuilder;
+import com.andorrero.amaristestjava.model.dto.PriceEntity;
+import com.andorrero.amaristestjava.repository.PriceRepository;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +25,29 @@ class EndPointTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
+
+    @Autowired
+    private PriceRepository priceRepository;
+
+    @BeforeEach
+    void setUp() {
+        priceRepository.save(new PriceEntity(PriceEntityBuilder.builder().brandId(1)
+                .startDate(LocalDateTime.of(2020, Month.JUNE, 14, 0, 0, 0))
+                .endDate(LocalDateTime.of(2020, Month.DECEMBER, 31, 23, 59, 59))
+                .priceList(1).productId(35455).priority(0).price(35.50f).currency("EUR").build()));
+        priceRepository.save(new PriceEntity(PriceEntityBuilder.builder().brandId(1)
+                .startDate(LocalDateTime.of(2020, Month.JUNE, 14, 15, 0, 0))
+                .endDate(LocalDateTime.of(2020, Month.JUNE, 14, 18, 30, 59))
+                .priceList(2).productId(35455).priority(1).price(25.45f).currency("EUR").build()));
+        priceRepository.save(new PriceEntity(PriceEntityBuilder.builder().brandId(1)
+                .startDate(LocalDateTime.of(2020, Month.JUNE, 15, 0, 0, 0))
+                .endDate(LocalDateTime.of(2020, Month.JUNE, 15, 11, 0, 0))
+                .priceList(3).productId(35455).priority(1).price(30.50f).currency("EUR").build()));
+        priceRepository.save(new PriceEntity(PriceEntityBuilder.builder().brandId(1)
+                .startDate(LocalDateTime.of(2020, Month.JUNE, 15, 16, 0, 0))
+                .endDate(LocalDateTime.of(2020, Month.DECEMBER, 31, 23, 59, 59))
+                .priceList(4).productId(35455).priority(1).price(38.95f).currency("EUR").build()));
+    }
 
     @ParameterizedTest
     @CsvSource({
